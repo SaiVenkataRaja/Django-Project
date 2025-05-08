ad130bb9243faf0766f6df21ecb89c04198df37a
Sai Venkata Raja Paleti <saivenkataraja245@gmail.com>
2025-05-08 17:41:23 +0530

    2nd commit

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,16 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.members, name='members'),        # matches /members/
+#     path('<int:id>/', views.details, name='details'), # matches /members/1/
+#     path('members/', views.members, name='members')
+
+# ]
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name='members')
+    path('', views.all_members, name='all_members'),  # this matches /members/
+    path('<int:id>/', views.member_details, name='member_details'),  # this matches /members/1/
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.template import loader
+# from django.shortcuts import render
+# # from django.http import HttpResponse
+# # from django.template import loader
+
+# # def members(request):
+# #     return HttpResponse("Hello World !!")
+
+# from .models import Member
 
 # def members(request):
-#     return HttpResponse("Hello World !!")
+#     all_members = Member.objects.all() # Fetches all rows from the table
+#     return render(request, 'members/all_members.html', {'members': all_members})
 
-def members(request):
-    return render(request, 'members/home.html')
+
+# from django.shortcuts import render
+# from .models import Member
+
+# def members(request):
+#     all_members = Member.objects.all()
+#     return render(request, 'members/all_members.html', {'mymembers': all_members})
+
+# def details(request, id):
+#     member = Member.objects.get(id=id)
+#     return render(request, 'members/details.html', {'member':member})
+
+from django.shortcuts import render, get_object_or_404
+from .models import Member
+
+def all_members(request):
+    # Fetch all members from the database
+    members = Member.objects.all()
+    return render(request, 'members/all_members.html', {'members': members})
+
+def member_details(request, id):
+    # Fetch a single member by its ID
+    member = get_object_or_404(Member, pk=id)
+    return render(request, 'members/details.html', {'member': member})
